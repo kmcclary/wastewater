7bdba0c1d5f15d98d93ceddfc7fd4903c7afd0b0
Kyle McClary <kyle@microscape.xyz>
2025-01-08 19:25:19 -0800

    attempt

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import ThemeProvider from './utils/ThemeContext';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <ThemeProvider>
         <App />
       </ThemeProvider>
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
