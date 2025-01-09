84e26a8728ae9b1fdeb351c7ba6d71c57921129c
Kyle McClary <kyle@microscape.xyz>
2025-01-08 19:18:17 -0800

    attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
 import React, { useEffect } from 'react';
-import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
-
-
+import { Routes, Route, useLocation } from 'react-router-dom';
 import './css/style.css';
-
 import './charts/ChartjsConfig';
 
 // Import pages
 import Dashboard from './pages/Dashboard';
 
 function App() {
-
   const location = useLocation();
 
   useEffect(() => {
     document.querySelector('html').style.scrollBehavior = 'auto'
     window.scroll({ top: 0 })
     document.querySelector('html').style.scrollBehavior = ''
-  }, [location.pathname]); // triggered on route change
+  }, [location.pathname]);
 
   return (
-    <>
-      <Routes>
-        <Route exact path="/" element={<Dashboard />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route exact path="/" element={<Dashboard />} />
+    </Routes>
   );
 }
 
